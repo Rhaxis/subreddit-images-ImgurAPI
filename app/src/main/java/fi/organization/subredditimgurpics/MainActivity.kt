5786ca520af354b851f837d3e59cdaef42df94be
Rhaxis <ville.ekholm88@gmail.com>
2021-05-21 14:20:36 +0300

    Final build

--- app/src/main/java/fi/organization/subredditimgurpics/MainActivity.kt
+++ app/src/main/java/fi/organization/subredditimgurpics/MainActivity.kt
@@ -9,7 +9,6 @@ import android.graphics.Typeface
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
@@ -34,19 +33,18 @@ data class Content(var link: String? = null, var type: String? = null, var in_ga
 data class Response(var data: MutableList<Content>? = null)
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var searchText: EditText
+    lateinit var mySpinner: Spinner
     lateinit var searchButton: Button
     lateinit var searchRandom: Button
     lateinit var container: LinearLayout
     lateinit var scrollingView: ScrollView
     lateinit var notFound: TextView
-    lateinit var spinner: Spinner
     var containerHeight = 200
     var customTitle = "Imgur content from subreddit"
-    val itemsToShow = 50
     val clientID = "5ab4b9f856bbf8c"
-
+    var itemsToShow = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,12 +55,27 @@ class MainActivity : AppCompatActivity() {
         this.searchRandom = findViewById(R.id.searchRandom)
         this.searchButton = findViewById(R.id.searchButton)
         this.notFound = findViewById(R.id.notFound)
-        this.spinner = findViewById(R.id.spinner1)
-        spinner.prompt = "How many items to show"
         title = customTitle
         containerHeight = container.height - 200
+
+        // Spinner with a dropdown menu to handle how many items are shown and sets listener for when item is selected
+        mySpinner = findViewById<View>(R.id.mySpinner) as Spinner
+        val items = arrayOf(5, 10, 25, 50)
+        val adapter = ArrayAdapter(this, R.layout.spinner_list, items)
+        adapter.setDropDownViewResource(R.layout.spinner_list)
+        mySpinner.adapter = adapter
+        mySpinner.onItemSelectedListener = this
     }
 
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+    }
+
+    // Updates spinner with the chosen amount and sets itemsToShow variable accordingly
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val value = parent?.getItemAtPosition(position)
+        mySpinner.setSelection(position)
+        itemsToShow = Integer.parseInt(value.toString())
+    }
     // Called when pressing Go button. Search for images in a subreddit based on user input in the edit text field
     @RequiresApi(Build.VERSION_CODES.R)
     fun getInput(view: View) {
@@ -91,6 +104,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Handles JSON parsing with jacksonparser and returns a list of data
     fun parseJson(string: String?): MutableList<Content>? {
         val mp = ObjectMapper()
         val myObject: Response = mp.readValue(string, Response::class.java)
@@ -102,6 +116,7 @@ class MainActivity : AppCompatActivity() {
         return myObject.data?.filter { it.in_gallery == false }?.take(itemsToShow)?.toMutableList()
     }
 
+    // Function for http connection to imgur api returns the response in a string
     fun httpConnection(inputText: String): String? {
         val client = OkHttpClient()
         val url = URL("https://api.imgur.com/3/gallery/r/$inputText")
@@ -114,6 +129,7 @@ class MainActivity : AppCompatActivity() {
         return response.body?.string()
     }
 
+    // async function for http connection and result callback
     private fun downloadUrlAsync(activity: Activity, s: String, callback: (result: String?) -> Unit) {
         thread() {
             var result = httpConnection(s)
@@ -123,11 +139,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Updates the app ui and handles image, gif and video downloading and rescaling to avoid memory issues.
     @RequiresApi(Build.VERSION_CODES.R)
     private fun updateUI(contentList: MutableList<Content>?, callback: (result: Boolean) -> Unit) {
         var done = 0
-        Log.d("TEST", "$done")
-        Log.d("TEST", "${contentList?.size}")
         contentList?.forEach {
             val url = it.link
             val type = it.type
@@ -135,12 +150,14 @@ class MainActivity : AppCompatActivity() {
             val height = it.height
             val width = it.width
 
+            // Checks if the type is image
             if (url != null && type?.contains("image") == true) {
                 thread() {
                     var textView = TextView(this)
                     var imageView = ImageView(this)
                     textViewParameters(textView, title)
                     imageViewParameters(imageView, url)
+                    // More checking if the image type is gif
                     if (type.contains("gif")) {
                         runOnUiThread {
                             Glide.with(this).load(url).into(imageView)
@@ -149,6 +166,7 @@ class MainActivity : AppCompatActivity() {
                         var myBitmap: Bitmap? = null
                         myBitmap?.recycle()
                         val input: InputStream = URL(url).openStream()
+                        // Image scaling according to its size to reduce memory consumption
                         if (height != null && width != null) {
                             if (height > 5000 || width > 5000) {
                                 val o = BitmapFactory.Options()
@@ -180,6 +198,7 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
+                    // Adds imageview and textview to the scrollable container
                     runOnUiThread {
                         this.container.addView(textView)
                         this.container.addView(imageView)
@@ -191,6 +210,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+            // Checks if the type is video
             if (url != null && type?.contains("video") == true) {
                 var videoView = VideoView(this)
                 val textView = TextView(this)
@@ -201,6 +221,8 @@ class MainActivity : AppCompatActivity() {
                     video.start()
                 }
                 videoView.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+
+                // Adds videoview and textview to the scrollable container
                 this.container.addView(textView)
                 this.container.addView(videoView)
                 done++
@@ -212,22 +234,25 @@ class MainActivity : AppCompatActivity() {
         scrollingView.fullScroll((ScrollView.FOCUS_UP))
     }
 
-
+    // Contains a list of subreddits and returns a string for random reddit used in the random button
     fun randomReddit(): String {
         val randomReddit = listOf("javascript", "java", "machinelearning", "howtohack", "dogs", "cats", "birds", "corgi")
         return randomReddit.shuffled().take(1)[0]
     }
 
+    // Toggles buttons enabled / disabled. Set to disabled when the app is working and enabled once app has finished updating ui.
     fun buttonsEnabled(status: Boolean) {
         searchButton.isEnabled = status
         searchRandom.isEnabled = status
     }
 
+    // Clears views from memory to avoid any memory issues.
     fun clear() {
         container.removeAllViewsInLayout()
         container.removeAllViews()
     }
 
+    // Used to indicate if user has given an invalid reddit name.
     fun redditFound(status: Boolean) {
         if(status) {
             title = customTitle
@@ -240,6 +265,7 @@ class MainActivity : AppCompatActivity() {
         searchText.setText("")
     }
 
+    // Sets parameters for the text view above videos or images
     fun textViewParameters(textView: TextView, title: String? = null) {
         textView.typeface = Typeface.DEFAULT_BOLD;
         textView.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
@@ -248,11 +274,11 @@ class MainActivity : AppCompatActivity() {
         textView.text = title
     }
 
-    fun imageViewParameters(imageView: ImageView, url : String) {
+    // Sets parameters for the image views.
+    fun imageViewParameters(imageView: ImageView, url: String) {
         imageView.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
         (imageView.layoutParams as LinearLayout.LayoutParams).setMargins(0, 20, 0, 200)
         imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-
         imageView.adjustViewBounds = true
         imageView.isClickable = true
         imageView.setOnClickListener {
